d735c7504f8f39607c915fdf851e113db183a8c3
Tony Naydenov <naydenovtony@gmail.com>
2026-05-18 09:44:23 +0300

    fix: suppress LogBox warning for keep awake activation

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from 'expo-router'
+import { LogBox } from 'react-native'
 import { AuthProvider } from '../lib/auth'
+
+LogBox.ignoreLogs(['Unable to activate keep awake'])
 
 export default function RootLayout(): React.JSX.Element {
   return (
